f1da587f7593d06ddcefaa8cece8179329580bd7
Evan C <evdcush@protonmail.com>
2022-11-12 19:30:01 +0900

    EXTENSIONS: Code Highlight 2, added.

---
+++ Extensions/source.sh
@@ -0,0 +1,12 @@
+
+
+# Code Highlighter 2
+# ==================
+# Code snipper highlighter for LibreOffice; powered by Pygments.
+#   Date: 20221112
+#   Version: v2.4.2
+#   Source:
+#     Catalog: https://extensions.libreoffice.org/en/extensions/show/5814
+#     Code: https://github.com/jmzambon/libreoffice-code-highlighter
+wget https://extensions.libreoffice.org/assets/downloads/508/1661322793/codehighlighter2.oxt
+
