0e09fe0c60343428569845b01f80dadee3488b33
Evan C <evdcush@protonmail.com>
2022-11-12 20:27:14 +0900

    EXTENSIONS: yaru-theme added

--- Extensions/source.sh
+++ Extensions/source.sh
@@ -10,3 +10,13 @@
 #     Code: https://github.com/jmzambon/libreoffice-code-highlighter
 wget https://extensions.libreoffice.org/assets/downloads/508/1661322793/codehighlighter2.oxt
 
+
+# Yaru icon theme
+# ===============
+# A fullcolor icon pack for Libreoffice of Yaru, the awesome default Ubuntu theme.
+#   Date: 20221112
+#   Version: 2022-09-04
+#   Source:
+#     Catalog: https://extensions.libreoffice.org/en/extensions/show/1042
+#     Code: https://github.com/ubuntu/libreoffice-style-yaru-fullcolor
+wget https://extensions.libreoffice.org/assets/downloads/264/1662285635/yaru-theme.oxt
